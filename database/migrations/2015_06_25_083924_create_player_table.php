9650bba515d3d7c19c2981c936eddd53b357e11b
Curtis H <ch@curtish.me>
2015-06-25 09:45:56 +0100

    initial commit for pubhack4

---
+++ database/migrations/2015_06_25_083924_create_player_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayerTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('game_id');
+            $table->string('number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
